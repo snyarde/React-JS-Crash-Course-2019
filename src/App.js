921adc5cdbc95e2c866090f508655ed3fbe28bd7
snyarde <57542514+snyarde@users.noreply.github.com>
2019-12-17 10:50:19 -0500

    Creates Header component
    Creates Header component  and adds to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Todos from "./components/Todos";
+import Header from "./components/layout/Header";
 
 class App extends Component {
   state = {
@@ -45,6 +46,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Todos
           todos={this.state.todos}
           markComplete={this.markComplete}
