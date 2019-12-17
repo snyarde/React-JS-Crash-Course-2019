964f1b1bb07b387a06fd2658ebbbfe41a37f21b4
snyarde <57542514+snyarde@users.noreply.github.com>
2019-12-17 10:11:24 -0500

    Adds markComplete method
    Adds markComplete method to App, passes as prop to component. Lifts state changes to App.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
       {
         id: 2,
         title: "Dinner with wife",
-        completed: false
+        completed: true
       },
       {
         id: 3,
@@ -21,11 +21,21 @@ class App extends Component {
       }
     ]
   };
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
 
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }
