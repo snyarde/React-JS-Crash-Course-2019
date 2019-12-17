ce4c7e536557d888ad87498a97dc8376dfc36246
snyarde <57542514+snyarde@users.noreply.github.com>
2019-12-17 09:00:47 -0500

    Adds Todos component
    Adds Todos component and seeds data through setting state and passing props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
+import Todos from "./components/Todos";
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Take out the trash",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Dinner with wife",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Meeting with boss",
+        completed: false
+      }
+    ]
+  };
+
   render() {
     return (
       <div className="App">
-        <h1>Skittles</h1>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
